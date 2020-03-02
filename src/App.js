09f9e16b855b8759d68c38f31776d427d284056a
normanbos <normanbos80@gmail.com>
2020-03-02 13:32:54 +0100

    Add item-list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
 import React from 'react'
-import './App.css'
+import styled from 'styled-components'
 
-function App() {
-  return <div>Hello things</div>
+export default function App() {
+  return (
+    <AppGrid>
+      <ItemList autoScroll="true">{Items()}</ItemList>
+    </AppGrid>
+  )
+
+  function Items() {
+    const items = [
+      { Gegenstand: 'Bohrmaschine', Name: 'Fritz' },
+      { Gegenstand: 'Backform', Name: 'Hans' },
+      { Gegenstand: 'Fahrrad', Name: 'Klaus' },
+      { Gegenstand: 'Buch', Name: 'Anna' },
+      { Gegenstand: 'CD', Name: 'Marie' },
+      { Gegenstand: 'noch ein Buch', Name: 'Jan' },
+    ]
+    return (
+      <div>
+        {items.map(item => {
+          return (
+            <SingleCard>
+              <p style={{ fontWeight: 'bold' }}>{item.Gegenstand}</p>
+              <p>ausgeliehen an: {item.Name}</p>
+            </SingleCard>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
-export default App
+const SingleCard = styled.div`
+  margin: 5px;
+  background: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 10px 10px #0002;
+`
+
+const ItemList = styled.section`
+  background: white;
+  padding: 20px;
+  border-radius: 5px;
+  overflow-y: auto;
+  autoscroll: true;
+  scroll-behavior: smooth;
+`
+
+const AppGrid = styled.div`
+  display: grid;
+  grid-template-rows: auto 48px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  height: 100%;
+`
