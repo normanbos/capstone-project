9c3324a4a631d89d05bf043e9f001f092d00cb4b
normanbos <normanbos80@gmail.com>
2020-03-04 09:36:02 +0100

    Clean up styling

--- src/components/itemList.js
+++ src/components/itemList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import itemData from '../itemData'
 import ItemCard from './itemCard'
 
-export default function Items() {
+export default function ItemList() {
   return (
     <>
       <div>
